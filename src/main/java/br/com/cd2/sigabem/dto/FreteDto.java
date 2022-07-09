68f4c32729b7b66b37428c629bfe406834e4ae07
Lucas Fernando <lucasfernandolf38@gmail.com>
2022-07-09 14:24:23 -0300

    FreteDto

---
+++ src/main/java/br/com/cd2/sigabem/dto/FreteDto.java
@@ -0,0 +1,12 @@
+package br.com.cd2.sigabem.dto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class FreteDto {
+    private String nomeDestinatario;
+    private String cepOrigem;
+    private String cepDestino;
+    private Double peso;
+}
