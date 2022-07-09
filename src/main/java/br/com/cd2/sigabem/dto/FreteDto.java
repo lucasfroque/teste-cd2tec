807a05a95758c542def1ca09bd9c5478009349ab
Lucas Fernando <lucasfernandolf38@gmail.com>
2022-07-09 15:17:51 -0300

    CepResponse

--- src/main/java/br/com/cd2/sigabem/dto/FreteDto.java
+++ src/main/java/br/com/cd2/sigabem/dto/FreteDto.java
@@ -9,4 +9,46 @@ public class FreteDto {
     private String cepOrigem;
     private String cepDestino;
     private Double peso;
+
+    public FreteDto(String nomeDestinatario, String cepOrigem, String cepDestino, Double peso) {
+        this.nomeDestinatario = nomeDestinatario;
+        this.cepOrigem = cepOrigem;
+        this.cepDestino = cepDestino;
+        this.peso = peso;
+    }
+
+    public FreteDto() {
+    }
+
+    public String getNomeDestinatario() {
+        return nomeDestinatario;
+    }
+
+    public void setNomeDestinatario(String nomeDestinatario) {
+        this.nomeDestinatario = nomeDestinatario;
+    }
+
+    public String getCepOrigem() {
+        return cepOrigem;
+    }
+
+    public void setCepOrigem(String cepOrigem) {
+        this.cepOrigem = cepOrigem;
+    }
+
+    public String getCepDestino() {
+        return cepDestino;
+    }
+
+    public void setCepDestino(String cepDestino) {
+        this.cepDestino = cepDestino;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
 }
--- src/main/java/br/com/cd2/sigabem/model/Frete.java
+++ src/main/java/br/com/cd2/sigabem/model/Frete.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class Frete implements Serializable {
-    private final long serialVersionUID = 1L;
+    public static final long serialVersionUID = 1L;
 
     private Long id;
     private String nomeDestinatario;
@@ -15,4 +15,82 @@ public class Frete implements Serializable {
     private LocalDateTime dataConsulta;
     private Double peso;
     private BigDecimal vlTotalFrete;
+
+    public Frete(Long id, String nomeDestinatario, String cepOrigem, String cepDestino, LocalDateTime dataPrevistaEntrega, LocalDateTime dataConsulta, Double peso, BigDecimal vlTotalFrete) {
+        this.id = id;
+        this.nomeDestinatario = nomeDestinatario;
+        this.cepOrigem = cepOrigem;
+        this.cepDestino = cepDestino;
+        this.dataPrevistaEntrega = dataPrevistaEntrega;
+        this.dataConsulta = dataConsulta;
+        this.peso = peso;
+        this.vlTotalFrete = vlTotalFrete;
+    }
+
+    public Frete() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeDestinatario() {
+        return nomeDestinatario;
+    }
+
+    public void setNomeDestinatario(String nomeDestinatario) {
+        this.nomeDestinatario = nomeDestinatario;
+    }
+
+    public String getCepOrigem() {
+        return cepOrigem;
+    }
+
+    public void setCepOrigem(String cepOrigem) {
+        this.cepOrigem = cepOrigem;
+    }
+
+    public String getCepDestino() {
+        return cepDestino;
+    }
+
+    public void setCepDestino(String cepDestino) {
+        this.cepDestino = cepDestino;
+    }
+
+    public LocalDateTime getDataPrevistaEntrega() {
+        return dataPrevistaEntrega;
+    }
+
+    public void setDataPrevistaEntrega(LocalDateTime dataPrevistaEntrega) {
+        this.dataPrevistaEntrega = dataPrevistaEntrega;
+    }
+
+    public LocalDateTime getDataConsulta() {
+        return dataConsulta;
+    }
+
+    public void setDataConsulta(LocalDateTime dataConsulta) {
+        this.dataConsulta = dataConsulta;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public BigDecimal getVlTotalFrete() {
+        return vlTotalFrete;
+    }
+
+    public void setVlTotalFrete(BigDecimal vlTotalFrete) {
+        this.vlTotalFrete = vlTotalFrete;
+    }
 }
